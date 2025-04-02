ade9de09de07481b0374bc90406695a5c8b4aaef
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-03 00:09:29 +0800

    ask the user to put a statement.

--- basic_functions_python_batch_five_string_manipulation/input_statement_print_word_count.py
+++ basic_functions_python_batch_five_string_manipulation/input_statement_print_word_count.py
@@ -1,3 +1,5 @@
 # ask the user to put a statement.
+user_statement = input("Enter a complete statement: ") 
+
 # make a condition that will count the word in the statement.
 # print how many word in the statement.
