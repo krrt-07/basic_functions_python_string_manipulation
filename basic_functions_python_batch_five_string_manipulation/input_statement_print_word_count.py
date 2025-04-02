c2f586162defca214e54c58efcd588a56f7bf8d6
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-03 00:09:47 +0800

    make a condition that will count the word in the statement.

--- basic_functions_python_batch_five_string_manipulation/input_statement_print_word_count.py
+++ basic_functions_python_batch_five_string_manipulation/input_statement_print_word_count.py
@@ -2,4 +2,6 @@
 user_statement = input("Enter a complete statement: ") 
 
 # make a condition that will count the word in the statement.
+word_count = len(user_statement.split())
+
 # print how many word in the statement.
