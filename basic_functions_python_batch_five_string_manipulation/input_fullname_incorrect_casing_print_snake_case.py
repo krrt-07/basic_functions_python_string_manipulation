6ae069871dbc7c2a9af64b0736e8caf34b6eb8ed
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-03 00:26:45 +0800

    make a condition that will make the fullname in lower casing and remove the spacing and change it to "_".

--- basic_functions_python_batch_five_string_manipulation/input_fullname_incorrect_casing_print_snake_case.py
+++ basic_functions_python_batch_five_string_manipulation/input_fullname_incorrect_casing_print_snake_case.py
@@ -2,4 +2,6 @@
 user_fullname = input("Enter your full name (incorrect casing): ") 
 
 # make a condition that will make the fullname in lower casing and remove the spacing and change it to "_".
+snake_case = user_fullname.lower().replace(" ", "_")  
+
 # print the fullname in snake case.
