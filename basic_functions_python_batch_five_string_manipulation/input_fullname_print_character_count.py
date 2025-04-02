7e631fc6deb6de5593087ac7a24bbdecef81f3bf
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-03 00:15:09 +0800

    make a condition that will count the letters in his/her fullname.

--- basic_functions_python_batch_five_string_manipulation/input_fullname_print_character_count.py
+++ basic_functions_python_batch_five_string_manipulation/input_fullname_print_character_count.py
@@ -2,4 +2,6 @@
 user_fullname = input("Enter your full name: ") 
 
 # make a condition that will count the letters in his/her fullname.
+char_count = len(user_fullname) 
+
 # print how many letters in his/her fullname.
