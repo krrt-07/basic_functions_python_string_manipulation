33c0cffac2b612b2d190de31222b738e374f01ac
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-03 00:20:31 +0800

    pseudocode made for making the fullname into pascal case.

---
+++ basic_functions_python_batch_five_string_manipulation/input_fullname_incorrect_casing_print_pascal_case.py
@@ -0,0 +1,3 @@
+# ask the user to input their fullname in incorrect casing.
+# make a condition that will make the fullname in proper casing and remove the spacing.
+# print the fullname in pascal case.
