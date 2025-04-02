6091bed1fb2f3ca77ecf24a41c5a69582d999d38
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-03 00:26:22 +0800

    sk the user to input their fullname in incorrect casing.

--- basic_functions_python_batch_five_string_manipulation/input_fullname_incorrect_casing_print_snake_case.py
+++ basic_functions_python_batch_five_string_manipulation/input_fullname_incorrect_casing_print_snake_case.py
@@ -1,3 +1,5 @@
 # ask the user to input their fullname in incorrect casing.
+user_fullname = input("Enter your full name (incorrect casing): ") 
+
 # make a condition that will make the fullname in lower casing and remove the spacing and change it to "_".
 # print the fullname in snake case.
