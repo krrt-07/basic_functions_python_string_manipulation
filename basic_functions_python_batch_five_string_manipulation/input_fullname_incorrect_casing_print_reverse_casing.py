95213719e40ebe82bd624e613dd5fe583f1a381c
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-03 00:01:51 +0800

    ask the user his/her fullname shuffled in upper and lower case.

--- basic_functions_python_batch_five_string_manipulation/input_fullname_incorrect_casing_print_reverse_casing.py
+++ basic_functions_python_batch_five_string_manipulation/input_fullname_incorrect_casing_print_reverse_casing.py
@@ -1,3 +1,5 @@
 # ask the user his/her fullname shuffled in upper and lower case.
+user_fullname = input("Enter your full name (incorrect casing): ")
+
 # make a condition that will make the fullname swap from lower to upper and upper to lower.
 # print the reversed fullname.
