c2b7dbd9152263313a45d5f5e1f480551f62bd05
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-03 00:26:00 +0800

    pseudocode made for making the fullname in snake case.

---
+++ basic_functions_python_batch_five_string_manipulation/input_fullname_incorrect_casing_print_snake_case.py
@@ -0,0 +1,3 @@
+# ask the user to input their fullname in incorrect casing.
+# make a condition that will make the fullname in lower casing and remove the spacing and change it to "_".
+# print the fullname in snake case.
