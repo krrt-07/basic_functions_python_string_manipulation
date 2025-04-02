0f3aaae7a544ac83a6174988057d1f89ac84b34e
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-03 00:20:55 +0800

    ask the user to input their fullname in incorrect casing.

--- basic_functions_python_batch_five_string_manipulation/input_fullname_incorrect_casing_print_pascal_case.py
+++ basic_functions_python_batch_five_string_manipulation/input_fullname_incorrect_casing_print_pascal_case.py
@@ -1,3 +1,5 @@
 # ask the user to input their fullname in incorrect casing.
+user_fullname = input("Enter your full name (incorrect casing): ") 
+
 # make a condition that will make the fullname in proper casing and remove the spacing.
 # print the fullname in pascal case.
