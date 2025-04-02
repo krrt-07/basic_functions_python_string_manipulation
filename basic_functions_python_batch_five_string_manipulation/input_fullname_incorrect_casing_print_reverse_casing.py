520d1b2186d937077c347eeefdaad3ec838bbf7d
Jacin kurt <jacinkurt.ocampo06@gmail.com>
2025-04-03 00:01:16 +0800

    pseudocode made to reversed the upper and lower in a shuffled fullname.

---
+++ basic_functions_python_batch_five_string_manipulation/input_fullname_incorrect_casing_print_reverse_casing.py
@@ -0,0 +1,3 @@
+# ask the user his/her fullname shuffled in upper and lower case.
+# make a condition that will make the fullname swap from lower to upper and upper to lower.
+# print the reversed fullname.
